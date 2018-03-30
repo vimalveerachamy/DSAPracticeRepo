6bea7efb3dbf9991dfc1c0743ae2c088a6b646a0
Vimal V <vkveera@APPL2PTRDKG8WP.i.ameriprise.com>
2018-03-30 12:51:02 -0500

    Added TwoSum algorithm

---
+++ DSAlgorithmStudy/DSAlgorithmStudy/TwoSum.swift
@@ -0,0 +1,44 @@
+//
+//  TwoSum .swift
+//  DSAlgorithmStudy
+//
+//  Created by Veerachamy, Vimal on 3/30/18.
+//  Copyright © 2018 Veerachamy, Vimal. All rights reserved.
+//
+
+/**
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+ 
+**/
+
+import Foundation
+
+func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    
+    for(index, element) in nums.enumerated(){
+        
+        let nextIndex = index + 1
+        
+        if(nextIndex < nums.count) {
+            
+            for j in nextIndex..<nums.count {
+                
+                let resultSum = element + nums[j]
+                
+                if(resultSum == target){
+                    return [index, j]
+                }
+            }
+        }
+    }
+    
+    return []
+}
