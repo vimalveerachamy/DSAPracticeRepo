89c222858da7412e2cee83555a0b105aeedaac3f
Vimal Veerachamhy <Vimal.Veerachamy@gmail.com>
2025-03-03 15:33:28 -0600

    Leetcode: Medium:  2161. Partition Array According to Given Pivot

--- DSAlgorithmSolutions/DSAlgorithmSolutions/Strings/PivotArray.swift
+++ DSAlgorithmSolutions/DSAlgorithmSolutions/Strings/PivotArray.swift
@@ -1,7 +1,23 @@
 //
-//  Untitled.swift
-//  
+//  PivotArray.swift
+//
 //
 //  Created by Vimal Veerachamy on 3/3/25.
 //
 
+class Solution {
+    
+    func pivotArray(_ nums: [Int], _ pivot: Int) -> [Int] {
+        var leftNums = [Int]()
+        var equalNums = [Int]()
+        var rightNums = [Int]()
+        
+        for num in nums {
+            if num == pivot { equalNums.append(num) }
+            else if num < pivot { leftNums.append(num) }
+            else { rightNums.append(num) }
+        }
+        
+        return leftNums + equalNums + rightNums
+    }
+}
