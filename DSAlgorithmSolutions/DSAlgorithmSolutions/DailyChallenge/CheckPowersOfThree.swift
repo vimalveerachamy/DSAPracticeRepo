a177b9f31441a8d6e7b7b29154adc47af9e2981b
Vimal Veerachamhy <Vimal.Veerachamy@gmail.com>
2025-03-04 15:44:43 -0600

    1780. Check if Number is a Sum of Powers of Three

--- DSAlgorithmSolutions/DSAlgorithmSolutions/DailyChallenge/CheckPowersOfThree.swift
+++ DSAlgorithmSolutions/DSAlgorithmSolutions/DailyChallenge/CheckPowersOfThree.swift
@@ -1,8 +1,22 @@
 //
-//  Untitled.swift
+//  CheckPowersOfThree.swift
 //  DSAlgorithmSolutions
 //
 //  Created by Vimal Veerachamy on 3/4/25.
 //  Copyright © 2025 Veerachamy, Vimal. All rights reserved.
 //
 
+class Solution {
+    
+    func checkPowersOfThree(_ n: Int) -> Bool {
+        
+        var n = n
+        
+        while n != 0 {
+            if n % 3 == 2 { return false }
+            n = n/3
+        }
+        
+        return true
+    }
+}
