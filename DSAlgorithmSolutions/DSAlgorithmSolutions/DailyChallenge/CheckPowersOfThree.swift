4460af880a2bdc5ce3a3a140141b5070e8e98380
Vimal Veerachamhy <Vimal.Veerachamy@gmail.com>
2025-03-06 14:52:53 -0600

    2965. Find Missing and Repeated Values

--- DSAlgorithmSolutions/DSAlgorithmSolutions/DailyChallenge/CheckPowersOfThree.swift
+++ DSAlgorithmSolutions/DSAlgorithmSolutions/DailyChallenge/CheckPowersOfThree.swift
@@ -6,17 +6,15 @@
 //  Copyright © 2025 Veerachamy, Vimal. All rights reserved.
 //
 
-class Solution {
+
+func checkPowersOfThree(_ n: Int) -> Bool {
     
-    func checkPowersOfThree(_ n: Int) -> Bool {
-        
-        var n = n
-        
-        while n != 0 {
-            if n % 3 == 2 { return false }
-            n = n/3
-        }
-        
-        return true
+    var n = n
+    
+    while n != 0 {
+        if n % 3 == 2 { return false }
+        n = n/3
     }
+    
+    return true
 }
